9521e1699b9311a0ccf557d1a7eb8681ddfbc5ed
jreue <jreue@users.noreply.github.com>
2026-05-17 22:16:41 -0500

    Added ESPNowHelper registration for Scanner, Module and Travel messages

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
+#include <shared_hardware_config.h>
+
 #include "Arduino.h"
 #include "DFRobotDFPlayerMini.h"
+#include "EspNowHelper.h"
 #include "hardware_config.h"
 
 DFRobotDFPlayerMini myDFPlayer;
+EspNowHelper espNowHelper;
 
 /*
 001 - date change
@@ -12,6 +16,9 @@ DFRobotDFPlayerMini myDFPlayer;
 005 - travel 2
 
 */
+void handleScannerMessage(const ScannerMessage& msg);
+void handleShieldModuleMessage(const ShieldModuleMessage& msg);
+void handleTravelMessage(const DateMessage& msg);
 void playDateChangeEffect();
 void playDeviceConnectedEffect();
 void playDeviceCalibratedEffect();
@@ -36,15 +43,57 @@ void setup() {
   }
 
   Serial.println("DFPlayer Mini Online!");
-
-  Serial.printf("Volume Level: %d\n", VOLUME_LEVEL);
-  Serial.println("Playing Track 1");
+  Serial.printf("Setting Volume Level: %d\n", VOLUME_LEVEL);
   myDFPlayer.volume(VOLUME_LEVEL);
+
+  espNowHelper.begin(DEVICE_ID);
+  espNowHelper.registerScannerMessageHandler(handleScannerMessage);
+  espNowHelper.registerModuleMessageHandler(handleShieldModuleMessage);
+  espNowHelper.registerDateMessageHandler(handleTravelMessage);
 
   testEffects();
 }
 
 void loop() {
+}
+
+void handleScannerMessage(const ScannerMessage& msg) {
+  Serial.println("Handling Scanner Message:");
+  Serial.printf("  Scanner ID: %d\n", msg.deviceId);
+  Serial.printf("  Message Type: %d\n", msg.messageType);
+
+  if (msg.messageType == MSG_TYPE_CONNECT) {
+    Serial.println("Scanner Connected!");
+    playDeviceCalibratedEffect();
+  }
+}
+
+void handleShieldModuleMessage(const ShieldModuleMessage& msg) {
+  Serial.println("Handling Shield Module Message:");
+  Serial.printf("  Shield Module ID: %d\n", msg.deviceId);
+  Serial.printf("  Message Type: %d\n", msg.messageType);
+  Serial.printf("  Is Calibrated: %s\n", msg.isCalibrated ? "Yes" : "No");
+
+  if (msg.messageType == MSG_TYPE_CONNECT) {
+    Serial.println("Shield Module Connected!");
+    playDeviceConnectedEffect();
+  } else if (msg.messageType == MSG_TYPE_DATA) {
+    if (msg.isCalibrated) {
+      Serial.println("Shield Module Calibrated!");
+      playDeviceCalibratedEffect();
+    }
+  }
+}
+
+void handleTravelMessage(const DateMessage& msg) {
+  Serial.println("Handling Travel Message:");
+  Serial.printf("  Date Module ID: %d\n", msg.deviceId);
+  Serial.printf("  Message Type: %d\n", msg.messageType);
+  Serial.printf("  Date: %02d/%02d/%04d\n", msg.month, msg.day, msg.year);
+
+  if (msg.messageType == MSG_TYPE_DATA) {
+    playTravel1Effect();
+  }
 }
 
 void testEffects() {
