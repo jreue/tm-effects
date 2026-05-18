f9139aecfeb1124f436b36820b4fab0c6083cb00
jreue <jreue@users.noreply.github.com>
2026-05-17 21:55:25 -0500

    More initial setup

--- src/hardware_config.h
+++ src/hardware_config.h
@@ -4,3 +4,8 @@
 // This Devices Configuration
 // ====================
 #define DEVICE_ID 40
+
+#define RX_PIN 16
+#define TX_PIN 17
+
+#define VOLUME_LEVEL 10  // Valid Range: 0-30
--- src/main.cpp
+++ src/main.cpp
@@ -1,140 +1,100 @@
 #include "Arduino.h"
 #include "DFRobotDFPlayerMini.h"
+#include "hardware_config.h"
 
-// =======================
-// DFPlayer Mini Pins
-// =======================
-#define RXD2 16
-#define TXD2 17
-
-// =======================
-// Button Pins
-// =======================
-#define BTN_PLAYPAUSE 22
-#define BTN_NEXT 21
-#define BTN_PREV 19
-#define BTN_VOLUP 18
-#define BTN_VOLDOWN 5
-
-// =======================
-// DFPlayer Object
-// =======================
 DFRobotDFPlayerMini myDFPlayer;
 
-// =======================
-// Variables
-// =======================
-int volumeLevel = 20;  // Volume range: 0-30
-bool isPaused = false;
+/*
+001 - date change
+002 - device connected
+003 - device calibrated
+004 - travel 1
+005 - travel 2
+
+*/
+void playDateChangeEffect();
+void playDeviceConnectedEffect();
+void playDeviceCalibratedEffect();
+void playTravel1Effect();
+void playTravel2Effect();
+void testEffects();
+void waitForEffectEnd(int track);
 
 void setup() {
-  // Start Serial Monitor
   Serial.begin(115200);
 
   // Start DFPlayer Serial Connection
-  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Serial2.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
 
   Serial.println("Initializing DFPlayer Mini...");
 
   // Connect to DFPlayer
   if (!myDFPlayer.begin(Serial2)) {
     Serial.println("DFPlayer Mini NOT detected!");
-    Serial.println("Check wiring and SD card.");
 
     while (true);
   }
 
   Serial.println("DFPlayer Mini Online!");
 
-  // =======================
-  // Button Setup
-  // =======================
-  pinMode(BTN_PLAYPAUSE, INPUT_PULLUP);
-  pinMode(BTN_NEXT, INPUT_PULLUP);
-  pinMode(BTN_PREV, INPUT_PULLUP);
-  pinMode(BTN_VOLUP, INPUT_PULLUP);
-  pinMode(BTN_VOLDOWN, INPUT_PULLUP);
-
-  // =======================
-  // Initial Settings
-  // =======================
-  myDFPlayer.volume(volumeLevel);
-
-  // Start playing first song
-  myDFPlayer.play(1);
-
+  Serial.printf("Volume Level: %d\n", VOLUME_LEVEL);
   Serial.println("Playing Track 1");
+  myDFPlayer.volume(VOLUME_LEVEL);
+
+  testEffects();
 }
 
 void loop() {
-  // =======================
-  // PLAY / PAUSE BUTTON
-  // =======================
-  if (digitalRead(BTN_PLAYPAUSE) == LOW) {
-    if (isPaused) {
-      myDFPlayer.start();
-      Serial.println("Resume Music");
-    } else {
-      myDFPlayer.pause();
-      Serial.println("Pause Music");
+}
+
+void testEffects() {
+  playDateChangeEffect();
+  waitForEffectEnd(1);
+
+  playDeviceConnectedEffect();
+  waitForEffectEnd(2);
+
+  playDeviceCalibratedEffect();
+  waitForEffectEnd(3);
+
+  playTravel1Effect();
+  waitForEffectEnd(4);
+
+  playTravel2Effect();
+  waitForEffectEnd(5);
+}
+
+void playDateChangeEffect() {
+  myDFPlayer.play(1);
+}
+
+void playDeviceConnectedEffect() {
+  myDFPlayer.play(2);
+}
+
+void playDeviceCalibratedEffect() {
+  myDFPlayer.play(3);
+}
+
+void playTravel1Effect() {
+  myDFPlayer.play(4);
+}
+
+void playTravel2Effect() {
+  myDFPlayer.play(5);
+}
+
+void waitForEffectEnd(int track) {
+  Serial.printf("[waitForEffectEnd] Waiting for track %d...\n", track);
+  while (true) {
+    if (myDFPlayer.available()) {
+      uint8_t type = myDFPlayer.readType();
+      int value = myDFPlayer.read();
+      Serial.printf("[waitForEffectEnd] Event type=%d value=%d\n", type, value);
+      if (type == DFPlayerPlayFinished && value == track) {
+        Serial.printf("[waitForEffectEnd] Track %d finished.\n\n", track);
+        break;
+      }
     }
-
-    isPaused = !isPaused;
-
-    delay(300);  // debounce
-  }
-
-  // =======================
-  // NEXT TRACK BUTTON
-  // =======================
-  if (digitalRead(BTN_NEXT) == LOW) {
-    myDFPlayer.next();
-
-    Serial.println("Next Track");
-
-    delay(300);  // debounce
-  }
-
-  // =======================
-  // PREVIOUS TRACK BUTTON
-  // =======================
-  if (digitalRead(BTN_PREV) == LOW) {
-    myDFPlayer.previous();
-
-    Serial.println("Previous Track");
-
-    delay(300);  // debounce
-  }
-
-  // =======================
-  // VOLUME UP BUTTON
-  // =======================
-  if (digitalRead(BTN_VOLUP) == LOW) {
-    if (volumeLevel < 30) {
-      volumeLevel++;
-
-      myDFPlayer.volume(volumeLevel);
-
-      Serial.print("Volume Up: ");
-      Serial.println(volumeLevel);
-    }
-
-    delay(200);  // debounce
-  }
-
-  // =======================
-  // VOLUME DOWN BUTTON
-  // =======================
-  if (digitalRead(BTN_VOLDOWN) == LOW) {
-    if (volumeLevel > 0) {
-      volumeLevel--;
-
-      myDFPlayer.volume(volumeLevel);
-
-      Serial.print("Volume Down: ");
-      Serial.println(volumeLevel);
-    }
-
-    delay(200);  // debounce
   }
 }
